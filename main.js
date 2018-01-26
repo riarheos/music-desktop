5874bcff5cfc3c327a27006bcb3446d648effa71
Artem Eltyshev <elty@yandex-team.ru>
2018-01-26 23:21:54 +0300

    lint fixes :)

--- main.js
+++ main.js
@@ -3,34 +3,33 @@ const path = require('path');
 
 let mainWindow = null;
 
-app.on('ready', function(){
+app.on('ready', function() {
     mainWindow = new BrowserWindow({
         width: 1024,
         height: 768,
         title: 'Yandex.Music',
-        icon: path.join(__dirname, "icon.png"),
+        icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
             nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
-    mainWindow.on('closed', function(){
+    mainWindow.on('closed', function() {
         app.quit();
     });
 
     mainWindow.loadURL('https://music.yandex.ru');
 
-    globalShortcut.register('mediaplaypause', function(){
+    globalShortcut.register('mediaplaypause', function() {
         mainWindow.webContents.send('playpause');
     });
 
-    globalShortcut.register('medianexttrack', function(){
+    globalShortcut.register('medianexttrack', function() {
         mainWindow.webContents.send('next');
     });
 
-    globalShortcut.register('mediaprevioustrack', function(){
+    globalShortcut.register('mediaprevioustrack', function() {
         mainWindow.webContents.send('prev');
     });
-
 });
