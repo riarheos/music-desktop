1541d720d5e74208388fa1eefc526a7a06860700
Stepan Ardalin <stepan@yandex.ru>
2018-06-03 15:40:44 +0300

    Autohide menu bar

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ const { app,
         BrowserWindow,
         globalShortcut } = require('electron');
 const { embedTouchBar } = require('./touchbar.js');
+const config = require('./config/config.js');
+
 
 app.on('ready', function() {
+    const hideMenuBar = config.get("hideMenuBar", false);
     const mainWindow = new BrowserWindow({
         width: 1024,
         height: 768,
         title: 'Yandex.Music',
-        autoHideMenuBar: true,
+        autoHideMenuBar: hideMenuBar,
         icon: path.join(__dirname, 'icon.png'),
         webPreferences: {
             nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js'),
         },
     });
+    require("./menu/mainmenu.js");
 
     mainWindow.on('closed', function() {
         app.quit();
