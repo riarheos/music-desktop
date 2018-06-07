3da7164d901345a84399c85bc0ef0345524af732
Stepan Ardalin <stepanar@yandex-team.ru>
2018-06-07 19:51:00 +0300

    Fix semicolons

--- menu/mainmenu.js
+++ menu/mainmenu.js
@@ -20,14 +20,18 @@ const template = [
                 label: 'Reload',
                 accelerator: 'CmdOrCtrl+R',
                 click(item, focusedWindow) {
-                    if (focusedWindow) focusedWindow.reload()
+                    if (focusedWindow) {
+                        focusedWindow.reload();
+                    }
                 }
             },
             {
                 label: 'Toggle Developer Tools',
                 accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
                 click(item, focusedWindow) {
-                    if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+                    if (focusedWindow) {
+                        focusedWindow.webContents.toggleDevTools();
+                    }
                 }
             },
             {
